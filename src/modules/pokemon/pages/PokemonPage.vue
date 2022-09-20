598a1f56da865f7c17309acd5f97497f9e4add1e
Jorge Ramos <v-jorramos@expediagroup.com>
2022-09-20 11:02:56 -0500

    Add name property to paths in router

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Pokemon page: {{ pokemonId }}</h1>
+  <div>
+    <h1>Pokemon page: {{ pokemonId }}</h1>
+    <div v-if="pokemon">
+      {{ pokemon.name }}
+      <br />
+      <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +15,33 @@ export default {
     pokemonId: {
       type: Number,
       required: true,
+    },
+  },
+  data() {
+    return {
+      pokemon: null,
+    };
+  },
+  created() {
+    this.getPokemon();
+  },
+  methods: {
+    async getPokemon() {
+      try {
+        const pokemon = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`
+        ).then(r => r.json());
+        this.pokemon = pokemon;
+        console.log(pokemon);
+      } catch (err) {
+        console.log(err);
+        this.$router.push('/');
+      }
+    },
+  },
+  watch: {
+    pokemonId() {
+      this.getPokemon();
     },
   },
 };
