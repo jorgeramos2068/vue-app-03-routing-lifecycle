7e9e3bebc3428af0c5fd591272033121a41830bf
Jorge Ramos <v-jorramos@expediagroup.com>
2022-09-23 16:27:57 -0500

    Add guard for specific pages

---
+++ src/router/authGuard.js
@@ -0,0 +1,14 @@
+const isAuthenticatedGuard = async (to, from, next) => {
+  return new Promise(() => {
+    const random = Math.random() * 100;
+    if (random > 50) {
+      console.log('Authenticated');
+      next();
+    } else {
+      console.log('Blocked by isAuthenticatedGuard:', random);
+      next({ name: 'pokemon-home' });
+    }
+  });
+};
+
+export default isAuthenticatedGuard;
